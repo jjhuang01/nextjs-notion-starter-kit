725bbc3bd6fd3cc10583a7e55c9d8e0bc05d2686
jjhuang01 <jjhuang01@outlook.com>
2022-12-05 04:15:01 +0800

    123

--- site.config.ts
+++ site.config.ts
@@ -42,11 +42,11 @@ export default siteConfig({
   // any pages defined here will override their default URL paths
   // example:
   //
-  // pageUrlOverrides: {
-  //   '/foo': '067dd719a912471ea9a3ac10710e7fdf',
-  //   '/bar': '0be6efce9daf42688f65c76b89f8eb27'
-  // }
-  pageUrlOverrides: null,
+  pageUrlOverrides: {
+    '/about': '9807e9619792431ea525f1920ac09178',
+    '/contact': 'dc6ed1e0137e40cab326db2465129b2f'
+  },
+  // pageUrlOverrides: null,
 
   // whether to use the default notion navigation style or a custom one with links to
   // important pages
