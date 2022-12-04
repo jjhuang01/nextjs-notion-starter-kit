654291cd94772823af5eb73cc181a2637eb4b9ca
jjhuang01 <jjhuang01@outlook.com>
2022-12-05 04:30:47 +0800

    change github

--- site.config.ts
+++ site.config.ts
@@ -6,7 +6,7 @@ export default siteConfig({
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
-  rootNotionSpaceId: null,
+  rootNotionSpaceId: 'workscpace-a0d12fc180f943bcb4600f278cd86d9a',
 
   // basic site info (required)
   name: '无双思绪飞扬',
