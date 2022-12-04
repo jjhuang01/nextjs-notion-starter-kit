d5584edade5ce930824e0ccd919348aa32a1600c
jjhuang01 <jjhuang01@outlook.com>
2022-12-05 03:48:49 +0800

    123

--- site.config.ts
+++ site.config.ts
@@ -38,7 +38,7 @@ export default siteConfig({
   // environment variables. see the readme for more info
   isRedisEnabled: false,
 
-  // map of notion page IDs to URL paths (optional)
+  // map of notion page IDs to URL paths  (optional)
   // any pages defined here will override their default URL paths
   // example:
   //
