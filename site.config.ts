db2ddad1d67e87b382ae35a35e0a177dfee9cc7b
jjhuang01 <56932508+jjhuang01@users.noreply.github.com>
2022-12-05 00:14:28 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,16 +2,16 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: 'df80aed6329c43f0b9b468439618ca64',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: '无双思绪飞扬',
+  domain: 'https://mangrove-twine-2ce.notion.site/workscpace-a0d12fc180f943bcb4600f278cd86d9a',
+  author: '无双',
 
   // open graph metadata (optional)
   description: 'Example Next.js Notion Starter Kit Site',
